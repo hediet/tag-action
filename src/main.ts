fafbdba12fb831df3768d9b0d36c36d482b3990f
Henning Dieterichs <henning.dieterichs@live.de>
2021-03-20 19:19:07 +0100

    Implements tag action.

--- src/main.ts
+++ src/main.ts
@@ -1,19 +1,48 @@
-import * as core from '@actions/core'
-import {wait} from './wait'
+import * as core from "@actions/core";
+import * as github from "@actions/github";
 
 async function run(): Promise<void> {
-  try {
-    const ms: string = core.getInput('milliseconds')
-    core.debug(`Waiting ${ms} milliseconds ...`) // debug is only output if you set the secret `ACTIONS_RUNNER_DEBUG` to true
+	try {
+		const githubToken = core.getInput("github_token", { required: true });
+		const tag = core.getInput("tag", { required: true });
+		const tagPrefix = core.getInput("tag_prefix", { required: false });
+		const create = core.getInput("create", { required: false }) === "true";
+		const failIfExists =
+			core.getInput("fail_if_exists", { required: false }) === "true";
 
-    core.debug(new Date().toTimeString())
-    await wait(parseInt(ms, 10))
-    core.debug(new Date().toTimeString())
+		const gitTag = tagPrefix + tag;
+		const gh = github.getOctokit(githubToken);
 
-    core.setOutput('time', new Date().toTimeString())
-  } catch (error) {
-    core.setFailed(error.message)
-  }
+		const tagRef = `refs/tags/${gitTag}`;
+
+		const existingRefs = await gh.git.listMatchingRefs({
+			...github.context.repo,
+			ref: `tags/${gitTag}`,
+		});
+
+		core.debug(`Existing refs: ${JSON.stringify(existingRefs)}`);
+
+		const exists = existingRefs.data.some(d => d.ref === `tags/${gitTag}`);
+
+		if (exists && failIfExists) {
+			throw new Error(
+				`Tag "${gitTag}" exists, but it should not! (fail_if_exists is set)`
+			);
+		}
+
+		if (create && !exists) {
+			core.info(`Creating tag "${gitTag}".`);
+			await gh.git.createRef({
+				...github.context.repo,
+				ref: tagRef,
+				sha: github.context.sha,
+			});
+		}
+
+		core.setOutput("exists", JSON.stringify(exists));
+	} catch (error) {
+		core.setFailed(error.message);
+	}
 }
 
-run()
+run();
